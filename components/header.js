16e68a2b24b4472db8b6827fa21f0b2c221068e8
Ashassins <ashenassassinmc@gmail.com>
2023-03-30 13:26:14 -0400

    test, adding just header.js

--- components/header.js
+++ components/header.js
@@ -7,8 +7,8 @@ headerTemplate.innerHTML = `
 </style>
 <header>
     <script src=”https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js”></script>
-    <script type=”text/javascript” src=”https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.9.0/moment.min.js”></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+    <script type=”text/javascript” src=”https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.9.0/moment.min.js”></script>
     
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <div class="container-fluid">
